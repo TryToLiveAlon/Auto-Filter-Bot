6b135dc31e1f42534ee4b704f0b79e71fe38cde0
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-03-30 17:57:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://app.koyeb.com/deploy?name=auto-filter-bot&repository=TryToLiveAlon%2FAuto-Filter-Bot&branch=main&instance_type=free&instances_min=0&autoscaling_sleep_idle_delay=300&env%5BBOT_TOKEN%5D=7951780194%3AAAHQ1Gz3zFDuKgzzrkmvauRyozKTapq65cI")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
